33184cbf66170c9ecbb745d26f178bc39e561a85
wntr <wntreud@gmail.com>
2026-07-01 08:42:43 -0400

    Delete src directory

--- src/preload/preload-setup.js
+++
@@ -1,5 +0,0 @@
-const { contextBridge, ipcRenderer } = require('electron');
-
-contextBridge.exposeInMainWorld('__PSTREAM_SETUP__', {
-  saveDomain: (domain) => ipcRenderer.invoke('save-domain', domain),
-});
--- src/preload/preload-updater.js
+++
@@ -1,9 +0,0 @@
-const { contextBridge, ipcRenderer } = require('electron');
-
-contextBridge.exposeInMainWorld('updater', {
-  onProgress: (callback) => {
-    ipcRenderer.on('update-progress', (event, data) => {
-      callback(data);
-    });
-  },
-});
